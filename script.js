ffd8634b6d9f871dcea5688899e7e1de2665c6e4
Probity94 <70590008+Probity94@users.noreply.github.com>
2020-10-01 15:05:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+(function () {
+
+    var names = ["Promise", "Joshua", "Judith", "James", "Prbity", "Simeon", "Okore", "Amaka", "Chidinma", "Janeth"];
+    
+    for (var i = 0; i < names.length; i++) {
+    
+      var firstLetter = names[i].charAt(0).toLowerCase();
+    
+      
+      if (firstLetter === 'j') {
+        byeSpeaker.speak(names[i]);
+      } else {
+        helloSpeaker.speak(names[i]);
+      }
+    }
+    
+    })();
+    
